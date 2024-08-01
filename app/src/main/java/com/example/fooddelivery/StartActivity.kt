15a5ba3a7161fa286e2c6c69b44b1318b41e4649
Uliay8 <128229025+Uliay8@users.noreply.github.com>
2024-08-01 17:47:29 +0500

    start and splash

--- app/src/main/java/com/example/fooddelivery/StartActivity.kt
+++ app/src/main/java/com/example/fooddelivery/StartActivity.kt
@@ -1,20 +1,33 @@
 package com.example.fooddelivery
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.fooddelivery.databinding.ActivityStartBinding
 
-class MainActivity : AppCompatActivity() {
+class StartActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityStartBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityStartBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.goUserLoginBtn.setOnClickListener {
+            val intent = Intent(this@StartActivity, LoginUserActivity :: class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
