2bdbe420b2b34ef7329e34ab3d8d1ae47d65cf91
Uliay8 <128229025+Uliay8@users.noreply.github.com>
2024-08-03 18:55:25 +0500

    Main
    BottomNavigationView have some problems... I don't know how can I solve it (except using marginBottom)

--- app/src/main/java/com/example/fooddelivery/MainActivity.kt
+++ app/src/main/java/com/example/fooddelivery/MainActivity.kt
@@ -5,16 +5,51 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.fooddelivery.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        changeFragment(HomeFragment())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.home -> {
+                    changeFragment(HomeFragment())
+                }
+                R.id.card -> {
+                    changeFragment(CardFragment())
+                }
+                R.id.search -> {
+                    changeFragment(SearchFragment())
+                }
+                R.id.history -> {
+                    changeFragment(HistoryFragment())
+                }
+                R.id.profile -> {
+                    changeFragment(ProfileFragment())
+                }
+            }
+            return@setOnItemSelectedListener true
+        }
+    }
+
+    fun changeFragment(fragment : Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.commit()
     }
 }
